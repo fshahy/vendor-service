50a1e7ffd4e48b20b7e1abe60b8e7aa0bab1fd85
Farid Shahy <fshahy@gmail.com>
2020-11-05 14:23:38 +0330

    add maven project.

---
+++ src/main/java/ir/delifery/Food/DeliFery/Vendor/Service/FoodDeliFeryVendorServiceApplication.java
@@ -0,0 +1,13 @@
+package ir.delifery.Food.DeliFery.Vendor.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliFeryVendorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliFeryVendorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/delifery/Food/DeliFery/Vendor/Service/FoodDeliFeryVendorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.delifery.Food.DeliFery.Vendor.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliFeryVendorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
